0bf6250c2a1da4e0222a13098820ffb3788b2c80
migueln4 <miguel444666@gmail.com>
2018-11-27 16:08:53 +0100

    Enrutado y menú
    Personalización de las rutas y personalización del menú con routerLink.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/* ----- RUTAS ------ */
+//Hay que importar las rutas aquí para que luego se pueda ir redirigiendo correctamente.
+import{APP_ROUTING} from './app.routes'; //Se tiene que poner la ruta así porque se toma de forma relativa al lugar en el que estamos ahora mismo.
+
+/* ----- SERVICIOS ------ */
+
+
+/* ----- COMPONENTES ------ */
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -16,7 +24,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,29 @@
+//Este archivo tiene una forma prestablecida para figurar las rutas.
+
+import {RouterModule, Routes} from '@angular/router';
+
+import {HomeComponent} from './components/home/home.component';
+import {AboutComponent} from './components/about/about.component';
+import {HeroesComponent} from './components/heroes/heroes.component';
+
+const APP_ROUTES: Routes = [ //Lo que se encuentra dentro de esta constante es un array de rutas.
+    {path: 'home', component: HomeComponent}, //Todas las rutas que hacen falta movilizar deben importarse al comienzo del archivo.
+    {path: 'about', component: AboutComponent},
+    {path: 'heroes', component: HeroesComponent},
+    {path: '**',pathMatch:'full',redirectTo:'home'//Esto está para que, si no se encuentra la ruta que se establece, redirecciona a un lugar determinado
+    }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+/*Tal y como está hecho, coge de la ruta que se introduce en el navegador lo que se necesita para poder redirigir y mostrar el contenido adecuado. Sin embargo, si se quieren pasar parámetros por la URL a la vieja usanza, hay que poner lo siguiente:
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash:true});
+Eso introduce un /#/ entre la raíz y la ruta que se ha especificado. Esto es muy eficiente y se soporta por la mayoría de los navegadores web.
+
+Sin usar el hash puede que falle. Entonces, haría falta hacer modificaciones en la parte del servidor.
+
+Además, cuando no se utiliza el routing con hash, en el html debe aparecer lo siguiente dentro del <head>:
+<base href="/">
+
+Sin poner eso y sin usar el hash va a provocar fallos que se pueden ver por la consola.
+*/
