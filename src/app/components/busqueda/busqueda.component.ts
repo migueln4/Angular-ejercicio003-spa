cedd4894ba5c682a684137862a1f727f1b35e18e
migueln4 <miguel444666@gmail.com>
2018-11-28 14:42:27 +0100

    Hecho un buscador de héroes
    Hecho el buscador del navbar y algunas redirecciones de seguridad.

---
+++ src/app/components/busqueda/busqueda.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Heroe, HeroesService} from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+})
+export class BusquedaComponent implements OnInit {
+
+  heroes:Heroe[] = [];
+
+  textoBuscado:string;
+
+  constructor(private activatedRoute:ActivatedRoute,
+              private _heroeService: HeroesService,
+              private router:Router) {
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(parametros => {
+      if(parametros['texto'].length > 0) {
+        console.log(parametros['texto']);
+        this.textoBuscado = parametros['texto'];
+        this.heroes = this._heroeService.buscarHeroe(parametros['texto']);
+        console.log(this.heroes);
+      } else {
+        this.router.navigate(['/home']);
+      }
+    });
+
+  }
+
+}
--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {ActivatedRoute} from '@angular/router'; //Esto hay que importarlo para poder recibir parámetros por URL
+import {ActivatedRoute,Router} from '@angular/router'; //Esto hay que importarlo para poder recibir parámetros por URL
 import {Heroe, HeroesService} from '../../services/heroes.service'; //Me tengo que importar esto para poder utilizar la interfaz que he definido en el servicio 
 
 @Component({
@@ -10,29 +10,38 @@ export class HeroeComponent  {
 
   heroe:Heroe; //Esta es la propiedad con la que vamos a trabajar en la vista de los héroes.
 
+
       //Como ocurre en el compontente de los héroes, lo que se hace es crear una propiedad del tipo del objeto que se ha importado (ActivatedRoute)
   constructor(private activatedRoute:ActivatedRoute,
-              private _heroeService: HeroesService) {
+              private _heroeService: HeroesService,
+              private router:Router) {
 
     //params lo que hace es devolver un observador
       this.activatedRoute.params.subscribe(parametros => { 
         console.log(parametros['id']); //Se utiliza una función de flecha y, de esta forma, se obtiene exclusivamente el identificador del parámetro. Si se pusiera únicamente el parámetro, vendría como un objeto que tiene como propiedad un número de identificación. El nombre "id" en este caso se pone porque es exactamente lo que se ha puesto en el archivo app.routes.ts
+        
+        if(this.datoCorrecto(parametros['id'])) {
+          
+            this.heroe = this._heroeService.getHeroe(parametros['id']);
 
-        this.heroe = this._heroeService.getHeroe(parametros['id']);
-
-        console.log(this.heroe);
-      })
+              console.log(this.heroe);
+        } else {
+          this.redireccionaListaHeroes();
+        }
+      });
     
        }
 
-       imprimirCasa():string {
+       private datoCorrecto(entrada:string):boolean {
+          if(isNaN(parseInt(entrada)) || parseInt(entrada) >= this._heroeService.getHeroes().length ||  parseInt(entrada) < 0) {;
+            return false;
+          } else {
+            return true;
+          }
+       }
 
-         if(this.heroe.casa.toUpperCase() === "MARVEL" ) {
-                    console.log("img/assets/marvel.png") ;
-         } else if (this.heroe.casa.toUpperCase() === "DC") {
-                    console.log("img/assets/dc.png") ;
-         }
-         return "";
+       private redireccionaListaHeroes() {
+        this.router.navigate(['/heroes']);
        }
 
 }
